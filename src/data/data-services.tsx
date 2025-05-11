736e1da6ad61f7638a5b40c7664f63a3b353f2ed
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2025-05-11 22:25:57 +0700

    update data

--- src/data/data-services.tsx
+++ src/data/data-services.tsx
@@ -2,20 +2,20 @@ import { Layers, Palette, Server } from "lucide-react";
 
 export const services = [
   {
-    description: "Frontend Web Development",
-    content:
+    title: "FrontEnd",
+    description:
       "Crafting responsive and engaging user interfaces using React.js, Next.js, and Tailwind CSS. Experienced with libraries like ShadCN UI, DaisyUI, Radix UI, and Framer Motion.",
     icon: <Palette />,
   },
   {
-    description: "Backend Web Development",
-    content:
+    title: "BackEnd",
+    description:
       "Building scalable and efficient backends using Next.js API, REST APIs, tRPC, and Prisma ORM. Proficient in handling databases such as PostgreSQL, MySQL, and Supabase.",
     icon: <Server />,
   },
   {
-    description: "Fullstack Web Development",
-    content:
+    title: "FullStack",
+    description:
       "Delivering complete web solutions with the T3 Stack and Next.js, ensuring seamless integration between frontend and backend.",
     icon: <Layers />,
   },
